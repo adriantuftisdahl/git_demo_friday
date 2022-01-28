69b419398efe97e5484fa21eef578ca02c73fe8a
adriantuftisdahl <ati_37@hotmail.com>
2022-01-28 09:40:08 +0100

    First commit

---
+++ some_code.py
@@ -0,0 +1,15 @@
+parameter1 = 1
+parameter2 = 2
+parameter3 = 3
+parameter4 = 4
+parameter5 = 5
+parameter6 = 6
+
+
+
+
+def important_method():
+    return_statement = "SUCCESS"
+    return return_statement
+
+
